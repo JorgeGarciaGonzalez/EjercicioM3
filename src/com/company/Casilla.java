268401ff96f0325e525b2190e64f5de57c29ff16
Jorge GarciaGonzalez <jorge.garcia.gonzalez@cieep.com>
2021-01-11 19:37:15 +0100

    Master

---
+++ src/com/company/Casilla.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Casilla {
+    private final String[][] nombresGolosinas = { {"KitKat", "Chicles de fresa", "Lacasitos", "Palotes"},
+            {"Kinder Bueno", "Bolsa variada Haribo", "Chetoos", "Twix"},
+            {"Kinder Choco", "M&M'S", "Papa Delta", "Chicles de menta"},
+            {"Chicles Menta", "Crunch", "Milkybar", "KitKat Blanco"}};
+    private final double[][] precio = { {1.1, 0.8, 1.5, 0.9},
+            {1.8, 1, 1.2, 1},
+            {1.8, 1.3, 1.2, 0.8},
+            {1.5, 1.1, 1.1, 1.1}};
+    private int cantidad[][]=new int[4][4];
+
+    public Casilla(){
+        for (int i = 0; i < cantidad.length; i++) {
+            for (int j = 0; j < cantidad[i].length; j++) {
+                cantidad[i][j]= 5;
+            }
+        }
+    }
+    public int getCantidad(int fila,int columna) {
+        return cantidad[fila][columna];
+    }
+    public void setCantidad(int cantidad, int fila, int columna) {
+        this.cantidad[fila][columna] = cantidad;
+    }
+    public double getPrecio(int fila,int columna) {
+        return precio[fila][columna];
+    }
+    public String getNombresGolosinas(int fila,int columna) {
+        return nombresGolosinas[fila][columna];
+    }
+    public void disminuirCantidad(int fila, int columna){
+        cantidad[fila][columna]--;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,101 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        Casilla casilla=new Casilla();
+        boolean salir =false;
+        int eleccion=0;
+        float ventas=0;
+
+        do{
+            System.out.println("Dime una opción");
+            System.out.println("1. Pedir golosina");
+            System.out.println("2. Mostrar golosina");
+            System.out.println("3. Rellenar golosinas");
+            System.out.println("4. Apagar maquina");
+            try {
+                eleccion = scanner.nextInt();
+            }catch (Exception e){
+                scanner.nextLine();
+                System.out.println("Opción no valida");
+            }
+            if (eleccion>0 && eleccion<5){
+                switch (eleccion){
+                    case 1:
+                        ventas=pedirGolosina(casilla,ventas);
+                        break;
+                    case 2:
+                        mostrarGolosina(casilla);
+                        break;
+                    case 3:
+                        rellenarGolosina(casilla);
+                        break;
+                    case 4:
+                        System.out.println("El total recaudado por la maquina es: "+ventas+" €");
+                        salir=true;
+                        break;
+                }
+            }
+        }while (!salir);
+    }
+    public static float pedirGolosina(Casilla casilla, float ventas){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Dime la posicion");
+        try {
+            int posisicion = scanner.nextInt();
+            int fila = posisicion/10;
+            int columna = posisicion%10;
+            if (fila>=0 && fila<4&&columna>=0 && columna<4){
+                if (casilla.getCantidad(fila, columna)>0){
+                    System.out.println("Toma tu producto "+casilla.getNombresGolosinas(fila,columna));
+                    casilla.disminuirCantidad(fila,columna);
+                    ventas += casilla.getPrecio(fila,columna);
+                }else System.out.println("No queda producto");
+            }else System.out.println("Posicion no valida");
+        }catch (Exception e){
+            scanner.nextLine();
+            System.out.println("Dato no correcto");
+        }
+        return ventas;
+    }
+    public static void mostrarGolosina(Casilla casilla){
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if(casilla.getCantidad(i,j)>0){
+                    System.out.println("En la casilla "+i+""+j+" tienes: "+casilla.getNombresGolosinas(i,j)+" \n Y cuesta: "+casilla.getPrecio(i,j));
+                }
+            }
+        }
+    }
+    public static void rellenarGolosina(Casilla casilla){
+        Scanner scanner=new Scanner(System.in);
+        String contrasenya="";
+        System.out.println("Dime la contraseña");
+        try{
+            contrasenya= scanner.nextLine();
+        }catch (Exception e){
+            System.out.println("Dato no correcto");
+        }
+        if (contrasenya.equals("MaquinaExpendedora2020")){
+            System.out.println("Dime la posisicion");
+            try{
+                int posisicion= scanner.nextInt();
+                int fila = posisicion/10;
+                int columna = posisicion%10;
+                if (fila>=0 && fila<4&&columna>=0 && columna<4){
+                    System.out.println("Dime la cantidad a rellenar");
+                    int cantidad= scanner.nextInt();
+                    casilla.setCantidad(cantidad,fila,columna);
+                }else System.out.println("Posicion no valida");
+            }catch (Exception e){
+                scanner.nextLine();
+                System.out.println("Dato no correcto");
+            }
+        }else System.out.println("Contraseña incorrecta");
+    }
+}
